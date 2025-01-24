c49912f1f2a3fe10b764310b7a0a063bd746405c
PATNALA SHYAM RAM <93208535+ShyamInCode@users.noreply.github.com>
2025-01-24 15:53:11 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginBtn = document.getElementById('loginBtn');
 
     loginBtn.addEventListener('click', () => {
-        window.location.href = 'https://auto-dash-test-gxm7hw95w-patnala-shyam-rams-projects.vercel.app/login';
+        window.location.href = 'https://auto-dash-test-hwg7dyt1r-patnala-shyam-rams-projects.vercel.app/login';
     });
 
 });
