d9234e6234a13f09d1f8b5a15266d538fa25b42a
Shyam <shyam@Admins-MacBook-Pro.local>
2025-01-24 15:18:04 +0530

    message

---
+++ public/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginBtn = document.getElementById('loginBtn');
+
+    loginBtn.addEventListener('click', () => {
+        window.location.href = 'https://lionfish-app-j5ry3.ondigitalocean.app/login';
+    });
+
+});
