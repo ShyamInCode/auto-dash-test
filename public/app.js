0addd0498197a6a0621c45306c5d3d17b08eca7a
PATNALA SHYAM RAM <93208535+ShyamInCode@users.noreply.github.com>
2025-01-24 16:18:14 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginBtn = document.getElementById('loginBtn');
 
     loginBtn.addEventListener('click', () => {
-        window.location.href = 'https://auto-dash-test.vercel.app/login';
+        window.location.href = 'https://auto-dash-test.onrender.com/login';
     });
 
 });
